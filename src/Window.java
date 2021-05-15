acb53d47dd8bcb447a5f6bec0367b92e55784f2d
Vladislav Skachinsky <vskachinskyr@gmail.com>
2021-05-15 23:46:26 +0300

    s

--- src/Window.java
+++ src/Window.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Window extends JFrame {
 
-    public Window() throws AWTException {
+    public Window() {
         setTitle("Clicker_v2.0");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500,200);
@@ -13,7 +12,7 @@ public class Window extends JFrame {
         setVisible(true);
     }
 
-    public static void main(String[] args) throws AWTException {
+    public static void main(String[] args) {
         new Window();
     }
 }
